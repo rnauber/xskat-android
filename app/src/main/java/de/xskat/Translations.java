98fa24553ff91994a3613778ec40272c3388c057
jmizv <alexander.schulze@automationhero.ai>
2020-07-18 14:23:52 +0200

    Reformat code.

--- app/src/main/java/de/xskat/Translations.java
+++ app/src/main/java/de/xskat/Translations.java
@@ -6,132 +6,132 @@ public class Translations {
     // --------------------------------------------------------------------------------------
 
     private final static String[][] TRANSLATIONS = { // DE, EN
-            { "Null", "Null" }, // XT_Null
-            { "Karo", "Diamond" }, // XT_Karo
-            { "Herz", "Heart" }, // XT_Herz
-            { "Pik", "Spade" }, // XT_Pik
-            { "Kreuz", "Club" }, // XT_Kreuz
-            { "Grand", "Grand" }, // XT_Grand
-            { "Ramsch", "Ramsch" }, // XT_Ramsch
-            { "Sortiere für Null", "Sort for Null" }, // XT_Sortiere_fuer_Null
-            { "Sortiere normal", "Sort normally" }, // XT_Sortiere_normal
-            { "Gereizt bis", "You have bid" }, // XT_Gereizt_bis
-            { "Androido", "Androido" }, // XT_Androido
-            { "Androida", "Androida" }, // XT_Androida
-            { "spielt", "plays" }, // XT_spielt
-            { "Hand", "Hand" }, // XT_Hand
-            { "für", "for" }, // XT_fuer
-            { "Spitze", "Spitze" }, // XT_Spitze
-            { "Annehmen", "Accept" }, // XT_Annehmen
-            { "Schenken", "Concede" }, // XT_Schenken
-            { "Passe", "Pass" }, // XT_Passe
-            { "Im Skat", "Skat" }, // XT_Im_Skat
-            { "war", "was" }, // XT_war
-            { "ist", "is" }, // XT_ist
-            { "Eine Jungfrau!", "One virgin!" }, // XT_Eine_Jungfrau
-            { "Durchmarsch!", "Forced march!" }, // XT_Durchmarsch
-            { "Überreizt!", "Overbid!" }, // XT_Ueberreizt
-            { "Gegner nicht Schneider!", "Opponents not Schneider!" }, // XT_Gegner_nicht_Schneider
-            { "Gegner nicht schwarz!", "Opponents not schwarz!" }, // XT_Gegner_nicht_schwarz
-            { "Spitze verloren!", "Lost Spitze!" }, // XT_Spitze_verloren
-            { "Spieler", "Player" }, // XT_Spieler
-            { "Niemand", "Nobody" }, // XT_Niemand
-            { "gewinnt", "wins" }, // XT_gewinnt
-            { "verliert", "loses" }, // XT_verliert
-            { "das Nullspiel.", "the Null game." }, // XT_das_Nullspiel
-            { "den Ramsch.", "the Ramsch." }, // XT_den_Ramsch
-            { "schwarz.", "schwarz." }, // XT_schwarz
-            { "mit", "with" }, // XT_mit
-            { "Augen.", "points." }, // XT_Augen
-            { "Spielwert", "Game value" }, // XT_Spielwert
-            { "Ja", "Yes" }, // XT_Ja
-            { "Weiter", "Continue" }, // XT_Weiter
-            { "Dieses Programm ist freie Software;",
-                    "This program is free software;" }, // XT_Dieses_Programm_ist_freie_Software
-            { "es kann frei verbreitet werden.",
-                    "you can redistribute it freely." }, // XT_es_kann_frei_verbreitet_werden
-            { "Nur bei Handspielen kann Schneider",
-                    "Only when playing Hand you may" }, // XT_Nur_bei_Handspielen_kann_Schneider
-            { "oder schwarz angesagt werden.", "say Schneider or schwarz." }, // XT_oder_schwarz_angesagt_werden
-            { "Ouvert schließt schwarz angesagt ein", "Ouvert includes schwarz" }, // XT_Ouvert_schliesst_schwarz_angesagt_ein
-            { "(außer bei Null natürlich).", "(except when playing Null)." }, // XT_ausser_bei_Null_natuerlich
-            { "Hand?", "Hand?" }, // XT_HandFrage
-            { "Nein", "No" }, // XT_Nein
-            { "Spiel", "Game" }, // XT_Spiel
-            { "G/V", "W/L" }, // XT_GV
-            { "Löschen", "Delete" }, // XT_Loeschen
-            { "Über XSkat", "About XSkat" }, // XT_Ueber_XSkat
-            { "Optionen", "Options" }, // XT_Optionen
-            { "Letzter Stich", "Last trick" }, // XT_Letzter_Stich
-            { "Lösche", "Delete" }, // XT_Loesche
-            { "Spielstand", "game score" }, // XT_Spielstand
-            { "und Liste?", "and list?" }, // XT_und_Liste
-            { "Liste", "List" }, // XT_Liste
-            { "Protokoll", "Log" }, // XT_Protokoll
-            { "Schneider", "Schneider" }, // XT_Schneider
-            { "Schwarz", "Schwarz" }, // XT_Schwarz
-            { "Ouvert", "Ouvert" }, // XT_Ouvert
-            { "Spielen", "Play" }, // XT_Spielen
-            { "Revolution", "Revolution" }, // XT_Revolution
-            { "Du hast höher gereizt als der", "The value of your game" }, // XT_Du_hast_hoeher_gereizt_als_der
-            { "Wert des angesagten Spiels!", "is not high enough!" }, // XT_Wert_des_angesagten_Spiels
-            { "Null: 23, Hand: 35", "Null: 23, Hand: 35" }, // XT_Null_23_Hand_35
-            { "ouvert: 46, ouvert Hand: 59", "ouvert: 46, ouvert Hand: 59" }, // XT_ouvert_46_ouvert_Hand_59
-            { "Revolution: 92", "Revolution: 92" }, // XT_Revolution_92
-            { "Drücken", "Done" }, // XT_Druecken
-            { "Fertig", "Done" }, // XT_Fertig
-            { "Spielstärke", "Playing strength" }, // XT_Spielstaerke
-            { "Sprache", "Language" }, // XT_Sprache
-            { "Display irgendwo antippen,", "Tap screen anywhere" }, // XT_Display_irgendwo_antippen
-            { "um Stich zu entfernen.", "to remove trick." }, // XT_um_Stich_zu_entfernen
-            { "Neues Spiel", "New game" }, // XT_NeuesSpiel
-            { "http://xskat.de/android-de.html", "http://xskat.de/android.html" }, // XT_xskat_de
-            { "Blatt", "Suit" }, // XT_Blatt
-            { "Turnier", "Tournament" }, // XT_Turnier
-            { "Französisch", "French" }, // XT_Franzoesisch
-            { "Deutsch", "German" }, // XT_Deutsch
-            { "Schellen", "Bells" }, // XT_Schellen
-            { "Rot", "Hearts" }, // XT_Rot
-            { "Grün", "Leaves" }, // XT_Gruen
-            { "Eicheln", "Acorns" }, // XT_Eicheln
-            { "Sortierung", "Sort" }, // XT_Sortierung
-            { "Alternierend", "Alternating" }, // XT_Alternierend
-            { "Sequentiell", "Sequential" }, // XT_Sequentiell
-            { "Nimm Stich nach", "Take trick after" }, // XT_Nimm_Stich_nach
-            { "Antippen.", "tapping." }, // XT_Antippen
-            { " Sekunden.", " seconds." }, // XT_Sekunden
-            { "Varianten", "Variants" }, // XT_Varianten
-            { "Immer", "Always" }, // XT_Immer
-            { "Kontra", "Kontra" }, // XT_Kontra
-            { "ab 18", "if 18" }, // XT_Ab_18
-            { "Bock", "Bock" }, // XT_Bock
-            { "+Ramsch", "+Ramsch" }, // XT_Plus_Ramsch
-            { "zählt 2", "counts 2" }, // XT_Zaehlt_2
-            { "Schieberamsch", "Schieberamsch" }, // XT_Schieberamsch
-            { "Skat geht an", "Skat goes to" }, // XT_Skat_geht_an
-            { "letzten Stich", "last trick" }, // XT_letzten_Stich
-            { "Verlierer", "loser" }, // XT_Verlierer
-            { "Skat aufnehmen?", "Pick up Skat?" }, // XT_Skat_aufnehmen
-            { "Spielwert verdoppelt!", "Game value doubled!" }, // XT_Spielwert_verdoppelt
-            { "nimmt den Skat nicht auf.", "doesn't pick up the Skat." }, // XT_nimmt_den_Skat_nicht_auf
-            { "klopft.", "knocks." }, // XT_klopft
-            { "Klopfen", "Klopfen" }, // XT_Klopfen
-            { "Spielwert verdoppeln?", "Double game value?" }, // XT_Spielwert_verdoppeln
-            { "Buben", "Buben" }, // XT_Buben
-            { "Jacks", "Jacks" }, // XT_Jacks
-            { "Unter", "Unter" }, // XT_Unter
-            { "dürfen nicht geschoben werden.", "may not be passed on." }, // XT_duerfen_nicht_geschoben_werden
-            { "Re", "Re" }, // XT_Re
-            { "von ", "by " }, // XT_von
-            { "mit Kontra", "with Kontra" }, // XT_mit_Kontra
-            { " & ", " & " }, // XT_und
-            { "Vorschläge", "Hints" }, // XT_Vorschlaege
-            { "Nochmal", "Replay" }, // XT_Nochmal
-            { "Spiel wiederholen", "Replay game" }, // XT_Spiel_wiederholen
-            { "mit den Karten von", "with the cards from" }, // XT_mit_den_Karten_von
-            { "mir", "me" }, // XT_mir
-            { "Zurück", "Back" }, // XT_Zurueck
-            { "Start", "Start" } // XT_Start
+            {"Null", "Null"}, // XT_Null
+            {"Karo", "Diamond"}, // XT_Karo
+            {"Herz", "Heart"}, // XT_Herz
+            {"Pik", "Spade"}, // XT_Pik
+            {"Kreuz", "Club"}, // XT_Kreuz
+            {"Grand", "Grand"}, // XT_Grand
+            {"Ramsch", "Ramsch"}, // XT_Ramsch
+            {"Sortiere für Null", "Sort for Null"}, // XT_Sortiere_fuer_Null
+            {"Sortiere normal", "Sort normally"}, // XT_Sortiere_normal
+            {"Gereizt bis", "You have bid"}, // XT_Gereizt_bis
+            {"Androido", "Androido"}, // XT_Androido
+            {"Androida", "Androida"}, // XT_Androida
+            {"spielt", "plays"}, // XT_spielt
+            {"Hand", "Hand"}, // XT_Hand
+            {"für", "for"}, // XT_fuer
+            {"Spitze", "Spitze"}, // XT_Spitze
+            {"Annehmen", "Accept"}, // XT_Annehmen
+            {"Schenken", "Concede"}, // XT_Schenken
+            {"Passe", "Pass"}, // XT_Passe
+            {"Im Skat", "Skat"}, // XT_Im_Skat
+            {"war", "was"}, // XT_war
+            {"ist", "is"}, // XT_ist
+            {"Eine Jungfrau!", "One virgin!"}, // XT_Eine_Jungfrau
+            {"Durchmarsch!", "Forced march!"}, // XT_Durchmarsch
+            {"Überreizt!", "Overbid!"}, // XT_Ueberreizt
+            {"Gegner nicht Schneider!", "Opponents not Schneider!"}, // XT_Gegner_nicht_Schneider
+            {"Gegner nicht schwarz!", "Opponents not schwarz!"}, // XT_Gegner_nicht_schwarz
+            {"Spitze verloren!", "Lost Spitze!"}, // XT_Spitze_verloren
+            {"Spieler", "Player"}, // XT_Spieler
+            {"Niemand", "Nobody"}, // XT_Niemand
+            {"gewinnt", "wins"}, // XT_gewinnt
+            {"verliert", "loses"}, // XT_verliert
+            {"das Nullspiel.", "the Null game."}, // XT_das_Nullspiel
+            {"den Ramsch.", "the Ramsch."}, // XT_den_Ramsch
+            {"schwarz.", "schwarz."}, // XT_schwarz
+            {"mit", "with"}, // XT_mit
+            {"Augen.", "points."}, // XT_Augen
+            {"Spielwert", "Game value"}, // XT_Spielwert
+            {"Ja", "Yes"}, // XT_Ja
+            {"Weiter", "Continue"}, // XT_Weiter
+            {"Dieses Programm ist freie Software;",
+                    "This program is free software;"}, // XT_Dieses_Programm_ist_freie_Software
+            {"es kann frei verbreitet werden.",
+                    "you can redistribute it freely."}, // XT_es_kann_frei_verbreitet_werden
+            {"Nur bei Handspielen kann Schneider",
+                    "Only when playing Hand you may"}, // XT_Nur_bei_Handspielen_kann_Schneider
+            {"oder schwarz angesagt werden.", "say Schneider or schwarz."}, // XT_oder_schwarz_angesagt_werden
+            {"Ouvert schließt schwarz angesagt ein", "Ouvert includes schwarz"}, // XT_Ouvert_schliesst_schwarz_angesagt_ein
+            {"(außer bei Null natürlich).", "(except when playing Null)."}, // XT_ausser_bei_Null_natuerlich
+            {"Hand?", "Hand?"}, // XT_HandFrage
+            {"Nein", "No"}, // XT_Nein
+            {"Spiel", "Game"}, // XT_Spiel
+            {"G/V", "W/L"}, // XT_GV
+            {"Löschen", "Delete"}, // XT_Loeschen
+            {"Über XSkat", "About XSkat"}, // XT_Ueber_XSkat
+            {"Optionen", "Options"}, // XT_Optionen
+            {"Letzter Stich", "Last trick"}, // XT_Letzter_Stich
+            {"Lösche", "Delete"}, // XT_Loesche
+            {"Spielstand", "game score"}, // XT_Spielstand
+            {"und Liste?", "and list?"}, // XT_und_Liste
+            {"Liste", "List"}, // XT_Liste
+            {"Protokoll", "Log"}, // XT_Protokoll
+            {"Schneider", "Schneider"}, // XT_Schneider
+            {"Schwarz", "Schwarz"}, // XT_Schwarz
+            {"Ouvert", "Ouvert"}, // XT_Ouvert
+            {"Spielen", "Play"}, // XT_Spielen
+            {"Revolution", "Revolution"}, // XT_Revolution
+            {"Du hast höher gereizt als der", "The value of your game"}, // XT_Du_hast_hoeher_gereizt_als_der
+            {"Wert des angesagten Spiels!", "is not high enough!"}, // XT_Wert_des_angesagten_Spiels
+            {"Null: 23, Hand: 35", "Null: 23, Hand: 35"}, // XT_Null_23_Hand_35
+            {"ouvert: 46, ouvert Hand: 59", "ouvert: 46, ouvert Hand: 59"}, // XT_ouvert_46_ouvert_Hand_59
+            {"Revolution: 92", "Revolution: 92"}, // XT_Revolution_92
+            {"Drücken", "Done"}, // XT_Druecken
+            {"Fertig", "Done"}, // XT_Fertig
+            {"Spielstärke", "Playing strength"}, // XT_Spielstaerke
+            {"Sprache", "Language"}, // XT_Sprache
+            {"Display irgendwo antippen,", "Tap screen anywhere"}, // XT_Display_irgendwo_antippen
+            {"um Stich zu entfernen.", "to remove trick."}, // XT_um_Stich_zu_entfernen
+            {"Neues Spiel", "New game"}, // XT_NeuesSpiel
+            {"http://xskat.de/android-de.html", "http://xskat.de/android.html"}, // XT_xskat_de
+            {"Blatt", "Suit"}, // XT_Blatt
+            {"Turnier", "Tournament"}, // XT_Turnier
+            {"Französisch", "French"}, // XT_Franzoesisch
+            {"Deutsch", "German"}, // XT_Deutsch
+            {"Schellen", "Bells"}, // XT_Schellen
+            {"Rot", "Hearts"}, // XT_Rot
+            {"Grün", "Leaves"}, // XT_Gruen
+            {"Eicheln", "Acorns"}, // XT_Eicheln
+            {"Sortierung", "Sort"}, // XT_Sortierung
+            {"Alternierend", "Alternating"}, // XT_Alternierend
+            {"Sequentiell", "Sequential"}, // XT_Sequentiell
+            {"Nimm Stich nach", "Take trick after"}, // XT_Nimm_Stich_nach
+            {"Antippen.", "tapping."}, // XT_Antippen
+            {" Sekunden.", " seconds."}, // XT_Sekunden
+            {"Varianten", "Variants"}, // XT_Varianten
+            {"Immer", "Always"}, // XT_Immer
+            {"Kontra", "Kontra"}, // XT_Kontra
+            {"ab 18", "if 18"}, // XT_Ab_18
+            {"Bock", "Bock"}, // XT_Bock
+            {"+Ramsch", "+Ramsch"}, // XT_Plus_Ramsch
+            {"zählt 2", "counts 2"}, // XT_Zaehlt_2
+            {"Schieberamsch", "Schieberamsch"}, // XT_Schieberamsch
+            {"Skat geht an", "Skat goes to"}, // XT_Skat_geht_an
+            {"letzten Stich", "last trick"}, // XT_letzten_Stich
+            {"Verlierer", "loser"}, // XT_Verlierer
+            {"Skat aufnehmen?", "Pick up Skat?"}, // XT_Skat_aufnehmen
+            {"Spielwert verdoppelt!", "Game value doubled!"}, // XT_Spielwert_verdoppelt
+            {"nimmt den Skat nicht auf.", "doesn't pick up the Skat."}, // XT_nimmt_den_Skat_nicht_auf
+            {"klopft.", "knocks."}, // XT_klopft
+            {"Klopfen", "Klopfen"}, // XT_Klopfen
+            {"Spielwert verdoppeln?", "Double game value?"}, // XT_Spielwert_verdoppeln
+            {"Buben", "Buben"}, // XT_Buben
+            {"Jacks", "Jacks"}, // XT_Jacks
+            {"Unter", "Unter"}, // XT_Unter
+            {"dürfen nicht geschoben werden.", "may not be passed on."}, // XT_duerfen_nicht_geschoben_werden
+            {"Re", "Re"}, // XT_Re
+            {"von ", "by "}, // XT_von
+            {"mit Kontra", "with Kontra"}, // XT_mit_Kontra
+            {" & ", " & "}, // XT_und
+            {"Vorschläge", "Hints"}, // XT_Vorschlaege
+            {"Nochmal", "Replay"}, // XT_Nochmal
+            {"Spiel wiederholen", "Replay game"}, // XT_Spiel_wiederholen
+            {"mit den Karten von", "with the cards from"}, // XT_mit_den_Karten_von
+            {"mir", "me"}, // XT_mir
+            {"Zurück", "Back"}, // XT_Zurueck
+            {"Start", "Start"} // XT_Start
     };
 
     static final int XT_Null = 0;
